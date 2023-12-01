5eeae1e0ad4a282e29d55fb1479aad38c7c03a0e
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-01 13:00:18 +0100

    Created Article with attributes

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Title</h2>
+      <label htmlFor="input">Input</label>
+      <input id="input"></input>
+      <a className="article__link" href="https://www.neuefische.de">
+        Link
+      </a>
+    </article>
+  );
 }
